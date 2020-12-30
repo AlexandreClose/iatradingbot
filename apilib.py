75abe2a7affea04995f6e248c4b0d41594b1ddf9
clement tatangelo <gladarius@gmail.com>
2020-12-30 18:35:03 +0100

    change websockets for websocket

--- apilib.py
+++ apilib.py
@@ -1,32 +1,43 @@
 ####librairie de connexion avec xtb.
 
-import websockets
+import websocket
 import asyncio
 import json
 
 
 class apilib:
 
-	def __init__(self):
+	def __init__(self, ws=None):
 		#uri websocket xtb demo
 		self.uri =  "wss://ws.xtb.com/demo"
+		self.ws=ws
 
 	##connexion au serveur xtb
 	async def connection(self, user, password):
 		command='{"command" : "login","arguments": {"userId": "' + user + '","password": "' + password + '"}}'
 		return await self.sendAndReceive(command)
 		
+	async def getAllSymbols(self,ws):
+		command='{"command": "getAllSymbols"}'
+		return await self.sendAndReceive(command,ws)
+
+	def connectionClosed(self, ws):
+		ws.close()
 
 	
 	###envoie et recvoie les requêtes au serveur xtb
 	#retourne la websocket ouverte + la réponse du serveur
-	async def sendAndReceive(self, command):
+	async def sendAndReceive(self, command, ws=None):
 		uri = self.uri
-		async with websockets.connect(uri) as websocket:
-			sent = await websocket.send(command)
-			response = await websocket.recv()
-
-		return [websocket, response]
+		if ws == None:
+			ws = websocket.create_connection(uri)
+			sent =  ws.send(command)
+			response =  ws.recv()
+		else:
+			print ('hola')
+			sent =  ws.send(command)
+			response =  ws.recv()
+		return [ws, response]
 
 	
 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from apilib import *
 import asyncio
-import websockets
+import websocket
 
 def main():
     lib=apilib()
-    result = asyncio.get_event_loop().run_until_complete(lib.connection("11671830","Azerty123"))
+    result =asyncio.get_event_loop().run_until_complete(lib.connection("11671830","Azerty123"))
     print (result)
-
-
+    result=asyncio.get_event_loop().run_until_complete(lib.getAllSymbols(result[0]))
+    print(result)
+    lib.connectionClosed(result[0])
 if __name__ == "__main__":
     main()
 
