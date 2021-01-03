e60e1b7c9476ed26b776e662539e544956a10e0e
AlexandreClose <alexandre.close@gmail.com>
2021-01-03 16:56:01 +0100

    Refactor open stream webseocket methods to just one

--- apilib.py
+++ apilib.py
@@ -11,9 +11,12 @@ class xtbClient:
 		self.ws_stream_news=None
 		self.ws_stream_candles=None
 		self.ws_stream_keep_alive=None
+		self.ws_stream_trades=None
 		self.news=[]
 		self.candles=[]
 		self.keep_alives=[]
+		self.trades=[]
+
 
 	async def login(self, user, password):
 		command = {"command" : "login","arguments": {"userId": user ,"password": password}}
@@ -30,12 +33,12 @@ class xtbClient:
 		return await self.send_and_receive(command)
 
 	async def get_candles(self, symbol):
-		await self.open_websocket_stream_candles()
+		self.ws_stream_candles = await self.open_websocket_stream()
 		command = {"command": "getCandles", "streamSessionId": self.stream_session_id, "symbol": symbol}
 		return await self.send_and_receive_stream( command, self.ws_stream_candles, self.candles)
 
 	async def get_keep_alive(self ):
-		await self.open_websocket_stream_keep_alive()
+		self.ws_stream_keep_alive = await self.open_websocket_stream()
 		command = {
 			"command": "getKeepAlive",
 			"streamSessionId": self.stream_session_id
@@ -43,9 +46,14 @@ class xtbClient:
 		return await self.send_and_receive_stream( command, self.ws_stream_keep_alive, self.keep_alives)
 
 	async def get_news(self ):
-		await self.open_websocket_stream_news()
+		self.ws_stream_news = await self.open_websocket_stream()
 		command = {"command": "getNews","streamSessionId": self.stream_session_id}
 		return await self.send_and_receive_stream(command, self.ws_stream_news, self.news)
+
+	async def get_trades(self):
+		self.ws_stream_trades = await self.open_websocket_stream()
+		command = { "command": "getTrades","streamSessionId": self.stream_session_id}
+		return await self.send_and_receive_stream( command, self.ws_stream_trades, self.trades)
 
 	async def getStreamingTradeStatusStart(self):
 		command={"command": "getTradeStatus","streamSessionId": self.stream_session_id}
@@ -82,17 +90,10 @@ class xtbClient:
 		ws.close()
 
 	async def open_websocket(self):
-			self.ws = await websockets.connect(self.uri, max_size=1_000_000_000)
+		self.ws = await websockets.connect(self.uri, max_size=1_000_000_000)
 
-	async def open_websocket_stream_news(self):
-		self.ws_stream_news = await websockets.connect(self.uri_stream, max_size=1_000_000_000)
-
-	async def open_websocket_stream_candles(self):
-		self.ws_stream_candles = await websockets.connect(self.uri_stream, max_size=1_000_000_000)
-
-	async def open_websocket_stream_keep_alive(self):
-		self.ws_stream_keep_alive = await websockets.connect(self.uri_stream, max_size=1_000_000_000)
-
+	async def open_websocket_stream(self):
+		return await websockets.connect(self.uri_stream, max_size=1_000_000_000)
 
 	###envoie et recvoie les requêtes au serveur xtb
 		# retourne la websocket ouverte + la réponse du serveur
