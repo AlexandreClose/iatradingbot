aa7fc12f87ad3347b695bcad6e50c7e406bbc9de
AlexandreClose <alexandre.close@gmail.com>
2021-01-05 17:11:48 +0100

    Add a streaming solution for tracking in real time trades

--- apilib.py
+++ apilib.py
@@ -1,7 +1,10 @@
 import enum
+import time
+
 import websockets
 import json
 from logging_conf import log
+import asyncio
 
 class MODES(enum.Enum):
 	BUY = 0
@@ -31,12 +34,16 @@ class TIME_TYPE(enum.Enum):
 	PERIOD_W1 = 10080
 	PERIOD_MN1 = 43200
 
+
 class xtbClient:
+
+
 	def __init__(self):
 		self.stream_session_id=None
 		self.uri="wss://ws.xtb.com/demo"
 		self.uri_stream= "wss://ws.xtb.com/demoStream"
-		self.ws=None
+		self.ws_login=None
+		self.ws_trades=None
 		self.ws_stream_news=None
 		self.ws_stream_candles=None
 		self.ws_stream_keep_alive=None
@@ -44,35 +51,46 @@ class xtbClient:
 		self.news=[]
 		self.candles=[]
 		self.keep_alives=[]
-		self.trades=[]
+		self.trades=dict()
 		self.ws_stream_tick_prices_dict = dict() # key : 'BITCOIN' => value : websocket stream tick prices BITCOIN (...)
 		self.tick_prices_dict = dict() # key : 'BITCOIN' => value : tick prices BITCOIN (...)
 
+	async def init_websockets( self ):
+		self.ws_login=await self.open_websocket()
+		self.ws_stream_news=await self.open_websocket_stream()
+		self.ws_stream_candles=await self.open_websocket_stream()
+		self.ws_stream_keep_alive=await self.open_websocket_stream()
+		self.ws_stream_trades=await self.open_websocket_stream()
+
 
 	async def login(self, user, password):
+		await self.init_websockets()
 		command = {"command" : "login","arguments": {"userId": user ,"password": password}}
-		response = await self.send_and_receive(command)
+		response = await self.send_and_receive(command, self.ws_login)
 		assert response['status']==True
 		self.stream_session_id = response['streamSessionId']
 		log.info( "[LOGIN] : Success. Session open on XTB with stream session id %s", self.stream_session_id )
 		log.debug( response )
 
+		#first fill the trades data with existing trades
+		await self.fill_existing_trades( )
+
+		#open stream for news about trades
+		asyncio.create_task( self.track_trades_stream( ) )
 
 	async def get_all_symbols(self):
 		command = {"command": "getAllSymbols"}
-		return await self.send_and_receive(command)
+		return await self.send_and_receive(command, self.ws_trades)
 
 	async def get_calendar(self ):
 		command = {"command": "getCalendar"}
-		return await self.send_and_receive(command)
+		return await self.send_and_receive(command, self.ws_trades)
 
 	async def get_candles(self, symbol):
-		self.ws_stream_candles = await self.open_websocket_stream()
 		command = {"command": "getCandles", "streamSessionId": self.stream_session_id, "symbol": symbol}
 		return await self.send_and_receive_stream( command, self.ws_stream_candles, self.candles)
 
 	async def get_keep_alive( self ):
-		self.ws_stream_keep_alive = await self.open_websocket_stream()
 		command = {
 			"command": "getKeepAlive",
 			"streamSessionId": self.stream_session_id
@@ -92,14 +110,47 @@ class xtbClient:
 		await self.send_and_receive_stream( command, self.ws_stream_tick_prices_dict[symbol], self.tick_prices_dict[symbol])
 
 	async def get_news(self ):
-		self.ws_stream_news = await self.open_websocket_stream()
 		command = {"command": "getNews","streamSessionId": self.stream_session_id}
 		return await self.send_and_receive_stream(command, self.ws_stream_news, self.news)
 
-	async def get_trades(self):
-		self.ws_stream_trades = await self.open_websocket_stream()
+	async def fill_existing_trades(self):
+		command = {
+			"command": "getTrades",
+			"arguments": {
+				"openedOnly": True
+			}
+		}
+		existing_trades = await self.send_and_receive( command, websocket=self.ws_login )
+		for trade in existing_trades['returnData']:
+			self.trades[trade['order2']]=trade
+		log.debug( "[TRADES] existing : %s", self.trades)
+
+
+	async def track_trades_stream(self):
 		command = { "command": "getTrades","streamSessionId": self.stream_session_id}
-		return await self.send_and_receive_stream( command, self.ws_stream_trades, self.trades)
+		log.info( "[COMMAND] : %s", command )
+		await self.ws_stream_trades.send( json.dumps( command ) )
+		while True:
+			response = await self.ws_stream_trades.recv()
+			response = json.loads( response )['data']
+			if response['type'] == 0:
+				self.trades[response['position']]=response
+			if response['type'] == 2:
+				if response['closed']:
+					self.trades[response['position']]=response
+			self._remove_closed_trades( )
+			log.debug( '[TRADES] total : %s', self.trades )
+
+	async def get_symbol(self, symbol):
+		command = {
+			"command": "getSymbol",
+			"arguments": {
+				"symbol": symbol
+			}
+		}
+		response = await self.send_and_receive( command, self.ws_login )
+		log.debug( response['returnData'] )
+		return response['returnData']
 
 	async def trade_transaction(self, mode, trans_type, symbol, volume, stop_loss = 0, take_profit = 0, **opt_args  ):
 
@@ -109,8 +160,12 @@ class xtbClient:
 		assert all([val in accepted_values for val in opt_args.keys()])
 		stop_loss = float(stop_loss)
 		take_profit = float(take_profit)
-		print(volume)
 		self._check_volume( volume )
+		conversion_mode = {MODES.BUY.value: 'ask', MODES.SELL.value: 'bid'}
+
+		if "price" not in opt_args:
+			symbol_current = await self.get_symbol( symbol )
+			opt_args['price'] = symbol_current[conversion_mode[mode.value]]
 
 		#Basic infos for opening trade
 		infos = {
@@ -131,8 +186,9 @@ class xtbClient:
 				"tradeTransInfo": infos
 			}
 		}
-
-		return await self.send_and_receive( command )
+		response = await self.send_and_receive( command, self.ws_login )
+		log.debug( response )
+		return response
 
 	def _check_volume(self, volume):
 		if not isinstance(volume, float):
@@ -177,36 +233,26 @@ class xtbClient:
 	async def get_history_chart(self,timestart,timeend,period,symbol):
 		chartinfo={"end": timeend,"period": period,"start": timestart,"symbol": symbol,"ticks": 0}
 		command = {"command": "getChartRangeRequest","arguments": {"info": {"end": timeend,"period": period.value,"start": timestart,"symbol": symbol,"ticks": 0}}}
-		return await self.send_and_receive(command)
+		return await self.send_and_receive(command, self.ws_login)
 
 
 	def connectionClosed(self, ws):
 		ws.close()
 
 	async def open_websocket(self):
-		self.ws = await websockets.connect(self.uri, max_size=1_000_000_000)
+		return await websockets.connect(self.uri, max_size=1_000_000_000)
 
 	async def open_websocket_stream(self):
 		return await websockets.connect(self.uri_stream, max_size=1_000_000_000)
 
 	###envoie et recvoie les requêtes au serveur xtb
 		# retourne la websocket ouverte + la réponse du serveur
-	async def send_and_receive(self, json_command):
+	async def send_and_receive(self, json_command, websocket ):
 		command = json.dumps(json_command)
 		log.info( "[COMMAND] : " + command )
-		if self.ws is None:
-			await self.open_websocket()
-		try:
-			await self.ws.send(command)
-			response =  await self.ws.recv()
-			log.debug(response)
-			return json.loads( response )
-		except	Exception:
-			self.open_socket()
-			await self.ws.send(command)
-			response = await self.ws.recv()
-			log.debug(response)
-			return json.loads( response )
+		await websocket.send(command)
+		response = await websocket.recv()
+		return json.loads( response )
 
 	async def send_and_receive_stream(self, json_command, websocket, resp_array):
 		command = json.dumps(json_command)
@@ -216,4 +262,7 @@ class xtbClient:
 			response = await websocket.recv()
 			log.debug( response )
 			resp_array.append( json.loads( response ))
+
+	def _remove_closed_trades( self ):
+		self.trades = {k: v for k, v in self.trades.items() if v['closed'] == False}
 
--- main.py
+++ main.py
@@ -1,34 +1,40 @@
+import time
+
 import logging_conf
 from apilib import *
 import asyncio
 import datetime
 
-def main():
+async def mainProgram( ):
     client=xtbClient()
+    await client.login("11676157","TestTest123123")
+    #loop.create_task(client.get_tick_prices( "DASH", min_arrival_time= 2000 )) # Performs async call
+    # Open stream for BITCOIN crypto infos
+    #loop.create_task(client.get_tick_prices( "BITCOIN", min_arrival_time= 2000 )) # Performs async call
+    # Open stream for BITCOIN crypto infos
+    await client.trade_transaction( MODES.BUY, TRANS_TYPES.OPEN, 'DASH', 1,0,0, customComment="titi")
+    time.sleep(0.200)
+    await client.trade_transaction( MODES.BUY, TRANS_TYPES.OPEN, 'DASH', 1,0,0, customComment="titi")
+    time.sleep(0.200)
+    await client.trade_transaction( MODES.BUY, TRANS_TYPES.OPEN, 'DASH', 1,0,0, customComment="titi")
+    time.sleep(0.200)
+
+    dt_start = datetime.datetime(2021, 1, 4 ,5,22,00)
+    timestart = int((dt_start - datetime.datetime(1970, 1, 1)).total_seconds())*1000
+    dt_stop = datetime.datetime(2021, 1, 4 ,6,24,00)
+    timestop = int((dt_stop - datetime.datetime(1970, 1, 1)).total_seconds())*1000
+
+    asyncio.create_task(client.get_history_chart(timestart,timestop,TIME_TYPE.PERIOD_M1,"EURUSD"))
+
+
+
+def main():
 
     # Use asyncio to run sync and async functions
     loop = asyncio.get_event_loop()
 
     # Performs login
-    loop.run_until_complete( client.login("11676157","TestTest123123") ) # Performs sync call, and await result
-
-    # Open stream for DASH crypto infos
-    #loop.create_task(client.get_tick_prices( "DASH", min_arrival_time= 2000 )) # Performs async call
-    
-    # Open stream for BITCOIN crypto infos
-    #loop.create_task(client.get_tick_prices( "BITCOIN", min_arrival_time= 2000 )) # Performs async call
-    
-    # Open stream for BITCOIN crypto infos
-    #loop.create_task(client.trade_transaction( MODES.BUY, TRANS_TYPES.OPEN, 'DASH', 1,0,0)) # Performs async call
-    
-    #getChartHistory
-    dt_start = datetime.datetime(2021, 1, 4 ,5,22,00)
-    timestart = int((dt_start - datetime.datetime(1970, 1, 1)).total_seconds())*1000
-    dt_stop = datetime.datetime(2021, 1, 4 ,6,24,00)
-    timestop = int((dt_stop - datetime.datetime(1970, 1, 1)).total_seconds())*1000
-    loop.create_task(client.get_history_chart(timestart,timestop,TIME_TYPE.PERIOD_M1,"EURUSD"))
-    
-
+    loop.run_until_complete( mainProgram( ) ) # Performs sync call, and await result
 
     loop.run_forever()
 
